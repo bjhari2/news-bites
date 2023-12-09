6545635c6922cd8da16da3c4d5cd717625e120e3
bjhari2 <121340541+bjhari2@users.noreply.github.com>
2023-12-09 15:51:13 +0530

    Update NewsHouses.jsx

--- src/components/NewsHouses.jsx
+++ src/components/NewsHouses.jsx
@@ -7,62 +7,62 @@ export default function NewsHouses() {
     {
       name: "Times of India",
       homepage: "https://timesofindia.indiatimes.com/",
-      logo_url: "/img/toi.png",
+      logo_url: "/img/toi.webp",
     },
     {
       name: "NDTV",
       homepage: "https://www.ndtv.com/",
-      logo_url: "/img/ndtv.png",
+      logo_url: "/img/ndtv.webp",
     },
     {
       name: "India Today",
       homepage: "https://www.indiatoday.in/",
-      logo_url: "/img/indiatoday.png",
+      logo_url: "/img/indiatoday.webp",
     },
     {
       name: "CNN-News18",
       homepage: "https://www.news18.com/",
-      logo_url: "/img/cnn18.png",
+      logo_url: "/img/cnn18.webp",
     },
     {
       name: "The Hindu",
       homepage: "https://www.thehindu.com/",
-      logo_url: "/img/thehindu.png",
+      logo_url: "/img/thehindu.webp",
     },
     {
       name: "India TV",
       homepage: "https://www.indiatvnews.com/",
-      logo_url: "/img/indiatv.png",
+      logo_url: "/img/indiatv.webp",
     },
     {
       name: "Hindustan Times",
       homepage: "https://www.hindustantimes.com/",
-      logo_url: "/img/ht.png",
+      logo_url: "/img/ht.webp",
     },
     {
       name: "ABP News",
       homepage: "https://www.abplive.com/",
-      logo_url: "/img/abp.png",
+      logo_url: "/img/abp.webp",
     },
     {
       name: "BBC News",
       homepage: "https://www.bbc.com/news",
-      logo_url: "/img/bbc.png",
+      logo_url: "/img/bbc.webp",
     },
     {
       name: "CNN",
       homepage: "https://edition.cnn.com/",
-      logo_url: "/img/cnn.png",
+      logo_url: "/img/cnn.webp",
     },
     {
       name: "The New York Times",
       homepage: "https://www.nytimes.com/",
-      logo_url: "/img/nyt.png",
+      logo_url: "/img/nyt.webp",
     },
     {
       name: "Al Jazeera",
       homepage: "https://www.aljazeera.com/",
-      logo_url: "/img/alzazeera.png",
+      logo_url: "/img/alzazeera.webp",
     },
   ];
   return (
