a5eb4624bdcc66dc27006e81095e102c77a9535a
bjhari2 <121340541+bjhari2@users.noreply.github.com>
2023-12-11 08:20:53 +0530

    Turned into PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,47 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from "vite-plugin-pwa";
+
+const manifestForPlugIn = {
+  registerType: "prompt",
+  includeAssets: ["favicon.ico", "apple-touch-icon.png", "maskable-icon.png"],
+  manifest: {
+    name: "NewsBites",
+    description: "Get your daily dose of news here!",
+    icons: [
+      {
+        src: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png"
+      },
+      {
+        src: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png"
+      },
+      {
+        src: "/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+        purpose: "apple touch icon"
+      },
+      {
+        src: "/maskable-icon.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any maskable"
+      }
+    ],
+    theme_color: "#323232",
+    background_color: "#323232",
+    dsplay: "standalone",
+    start_url: "/",
+    scope: "/",
+    orientation: "portrait"
+  }
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA(manifestForPlugIn)],
 });
